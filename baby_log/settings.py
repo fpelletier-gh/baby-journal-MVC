51af2aa2f650e15241bd488da60c968e9cb9e367
Francis Pelletier <francispelletier2@hotmail.com>
2022-08-05 20:14:19 -0400

    Fix a warning for default auto field

--- baby_log/settings.py
+++ baby_log/settings.py
@@ -94,9 +94,15 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
-    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",},
-    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",},
-    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",},
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
 ]
 
 
@@ -121,3 +127,4 @@ STATIC_URL = "/static/"
 
 # My settings
 LOGIN_URL = "users:login"
+DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
