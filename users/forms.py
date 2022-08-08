3cfa668812d6cde87ba79e9ae612789cd0b3bb64
Francis Pelletier <francispelletier2@hotmail.com>
2022-08-08 11:51:06 -0400

    refactor register and login forms to remove labels

--- users/forms.py
+++ users/forms.py
@@ -1,18 +1,97 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import (
+    UserCreationForm,
+    AuthenticationForm,
+    UsernameField,
+)
 from django.contrib.auth.models import User
 
 
 class UserCreationFormWithEmail(UserCreationForm):
-    email = forms.EmailField(required=True, label="Email")
+    username = forms.CharField(
+        max_length=100,
+        required=True,
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "Username",
+                "class": "my-4",
+            }
+        ),
+        label="",
+    )
+
+    email = forms.EmailField(
+        required=True,
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "Email",
+                "class": "my-4",
+            },
+        ),
+        label="",
+    )
+
+    password1 = forms.CharField(
+        max_length=50,
+        required=True,
+        widget=forms.PasswordInput(
+            attrs={
+                "placeholder": "Password",
+                "class": "my-4",
+            }
+        ),
+        label="",
+    )
+
+    password2 = forms.CharField(
+        max_length=50,
+        required=True,
+        widget=forms.PasswordInput(
+            attrs={
+                "placeholder": "Confirm Password",
+                "class": "my-4",
+            }
+        ),
+        label="",
+    )
 
     class Meta:
         model = User
-        fields = ("username", "email", "password1", "password2")
+        fields = [
+            "username",
+            "email",
+            "password1",
+            "password2",
+        ]
 
-    def save(self, commit=True):
-        user = super(UserCreationForm, self).save(commit=False)
-        user.email = self.cleaned_data["email"]
-        if commit:
-            user.save()
-        return user
+
+class LoginForm(AuthenticationForm):
+    def __init__(self, *args, **kwargs):
+        super(LoginForm, self).__init__(*args, **kwargs)
+
+    username = UsernameField(
+        max_length=100,
+        required=True,
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "Username",
+                "class": "my-4",
+            }
+        ),
+        label="",
+    )
+    password = forms.CharField(
+        max_length=50,
+        required=True,
+        widget=forms.PasswordInput(
+            attrs={
+                "placeholder": "Password",
+                "class": "my-4",
+            }
+        ),
+        label="",
+    )
+
+    class Meta:
+        model = User
+        fields = ["username", "password"]
--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,20 @@
 """Defines URL patterns for baby_logs"""
 from django.urls import path, include
+from django.contrib.auth import views
+from .forms import LoginForm
 
-from . import views
+from . import views as users_views
 
 app_name = "users"
 urlpatterns = [
     # Default auth url
+    path(
+        "login/",
+        views.LoginView.as_view(
+            template_name="registration/login.html", authentication_form=LoginForm
+        ),
+        name="login",
+    ),
+    path("register/", users_views.register, name="register"),
     path("", include("django.contrib.auth.urls")),
-    path("register/", views.register, name="register"),
 ]
-
