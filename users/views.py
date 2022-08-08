bb1c8d557bf0aca2e9e9d8005f46ca03af737398
Francis Pelletier <francispelletier2@hotmail.com>
2022-08-08 13:48:30 -0400

    add success message after account creation

--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login
+from django.contrib import messages
 from .forms import UserCreationFormWithEmail
 
 
@@ -13,10 +13,11 @@ def register(request):
         form = UserCreationFormWithEmail(data=request.POST)
 
         if form.is_valid():
-            new_user = form.save()
-            # Log the user in and then redirect to home page.
-            login(request, new_user)
-            return redirect("baby_logs:index")
+            form.save()
+
+            username = form.cleaned_data.get("username")
+            messages.success(request, f"Account created for {username}")
+            return redirect("users:login")
     # Display a blank or invalid form.
     context = {"form": form}
     return render(request, "registration/register.html", context)
