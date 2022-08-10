19a726db494ea3465a5141148337f26a5e140643
Francis Pelletier <francispelletier2@hotmail.com>
2022-08-10 13:22:14 -0400

    refactor settings to single files with env

--- baby_log/wsgi.py
+++ baby_log/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "baby_log.settings.production")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "baby_log.settings")
 
 application = get_wsgi_application()
