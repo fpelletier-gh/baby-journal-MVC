42b3645dab5450a7162c144aad410aab94d8c1d2
Francis Pelletier <francispelletier2@hotmail.com>
2022-08-24 15:25:41 -0400

    add staticfiles folder for background image

--- baby_log/settings.py
+++ baby_log/settings.py
@@ -137,7 +137,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "baby_logs/static")]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "baby_logs/staticfiles")]
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
